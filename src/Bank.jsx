0538f86d11097206697af86f38a6a2a53db1e4b5
Harsh Tiwari <harsh.tiwari1995@gmail.com>
2021-09-20 13:27:36 +0530

    First commit

---
+++ src/Bank.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+
+const Bank = () => {
+    const [amount, setAmount] = useState(0)
+    const [flag, setFlag] = useState(false)
+    const [balance, setBalance] = useState(0)
+    const [state, setState] = useState(0)
+    function handleChange(e) {
+        setAmount(e.target.value)
+    }
+    function handleDeposit() {
+        setFlag(false)
+        setBalance(balance => parseFloat(balance) + parseFloat(amount))
+    }
+    useEffect(() => {
+        localStorage.setItem("balance", balance)
+    }, [balance, amount])
+    function handleWithdraw() {
+        setFlag(false)
+        if (balance - amount > 0)
+            setBalance(balance => parseFloat(balance) - parseFloat(amount))
+    }
+    function handleBalance(){
+        const remaining = localStorage.getItem("balance")
+        setState(remaining)
+        setFlag(true)
+    }
+    return (
+        <div>
+            <h1>&#8377;
+                {amount}</h1>
+            <div className="input-group mb-3">
+                <input type="text"
+                    className="form-control container my-3"
+                    placeholder="Enter amount to deposit or withdraw"
+                    value={amount}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="container ">
+                <button type="button" className="btn btn-primary mx-3" onClick={() => handleDeposit()}>Add Money</button>
+                <button type="button" className="btn btn-primary mx-3" onClick={() => handleWithdraw()} >Withdraw Money</button>
+                <button type="button" className="btn btn-primary mx-3" onClick={() => handleBalance()} >Fetch Balance</button>
+            </div>
+            <br />
+            <h4>Balance:&#8377;{flag?state:"****"}</h4>
+        </div>
+    )
+}
+
+export default Bank
